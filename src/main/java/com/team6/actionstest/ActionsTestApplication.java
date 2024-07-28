91978ff00bfc580e0f8073600783fd7dc0729e59
Seungwon_Choi <bestwondev@gmail.com>
2024-07-28 19:46:09 +0900

    Initial commit

---
+++ src/main/java/com/team6/actionstest/ActionsTestApplication.java
@@ -0,0 +1,13 @@
+package com.team6.actionstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActionsTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team6/actionstest/ActionsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team6.actionstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
