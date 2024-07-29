5a66f26d35095b04197e837b2f12e5b76e467f02
Seungwon_Choi <bestwondev@gmail.com>
2024-07-30 00:08:52 +0900

    Update SampleController.java

--- src/main/java/com/team6/actionstest/SampleController.java
+++ src/main/java/com/team6/actionstest/SampleController.java
@@ -1,13 +1,13 @@
 package com.team6.actionstest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
 
     // test
-    @RequestMapping("/sample")
+    @GetMapping("/sample")
     public String sample() {
         return "Hello World!";
     }
